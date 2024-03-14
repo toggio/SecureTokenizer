3a15b19860d781894d30dc2012320d3abd0d4fea
Luca Soltoggio <toggio78@gmail.com>
2024-03-14 16:15:08 +0100

    Update PraticalUseCaseSender.php

--- examples/PraticalUseCaseSender.php
+++ examples/PraticalUseCaseSender.php
@@ -13,7 +13,7 @@ echo $tokenizer->jsInit; // Print the JavaScript code for creating time-based to
     function myAjaxFunction() {
         // Prints "let var token=...;" - Code for generating JS time-based token
         <?php echo $tokenizer->jsToken; ?>
-        fetch('receiver.php?token='+token)
+        fetch('PraticalUseCaseReceiver.php?token='+token)
           .then(response => response.text()) 
           .then(text => document.write(text)) 
           .catch(error => console.error('Fetch error:', error));
